bbbfaeec32403931c01d7f184ed978ac9c17c0c8
Yuvi2429 <yuvarajyuvi2324@gmail.com>
2025-10-23 23:01:49 +0530

    Changes in server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080), 
+
+  proxy: env.bool('TRUST_PROXY', true),
+
+  url: env('PUBLIC_URL', 'https://strapi-526588758494.europe-west1.run.app'),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
