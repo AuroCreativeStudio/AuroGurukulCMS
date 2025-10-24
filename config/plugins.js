b45b966d16c6f543cc4f35b217d7e7bb7d9300c5
Yuvi2429 <yuvarajyuvi2324@gmail.com>
2025-10-24 14:42:17 +0530

    uploaded service account

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,24 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
-      providerOptions: {
-        bucketName: env("GCS_BUCKET_NAME"),
-        publicFiles: true,
-        uniform: false,
-        basePath: "",
-       serviceAccount: require("../gcs-service-account.json"),
+const fs = require("fs");
+const path = require("path");
+
+module.exports = ({ env }) => {
+  const serviceAccountPath = path.join(__dirname, "gcs-service-account.json");
+  const serviceAccount = fs.existsSync(serviceAccountPath)
+    ? require(serviceAccountPath)
+    : null;
+
+  return {
+    upload: {
+      config: {
+        provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+        providerOptions: {
+          bucketName: env("GCS_BUCKET_NAME"),
+          publicFiles: true,
+          uniform: false,
+          basePath: "",
+          serviceAccount,
+        },
       },
     },
-  },
-});
+  };
+};
