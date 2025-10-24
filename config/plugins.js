ad1af3d055f7c4bad31ac263d0b60da39653bac4
Yuvi2429 <yuvarajyuvi2324@gmail.com>
2025-10-24 12:38:35 +0530

    modified in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+      providerOptions: {
+        bucketName: env("GCS_BUCKET_NAME"),
+        publicFiles: true,
+        uniform: false,
+        basePath: "", // optional
+        serviceAccount: env("GCS_SERVICE_ACCOUNT_JSON"),
+      },
+    },
+  },
+});
