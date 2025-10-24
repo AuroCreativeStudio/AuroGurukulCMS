db20f20d32282d6aeaa4b6feb49fdc764b293a33
Yuvi2429 <yuvarajyuvi2324@gmail.com>
2025-10-24 15:36:45 +0530

    Plugin.js Modification

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,20 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
-      providerOptions: {
-        bucketName: env("GCS_BUCKET_NAME"),
-        publicFiles: true,
-        uniform: false,
-        basePath: "",
-        serviceAccount: JSON.parse(
-          Buffer.from(env("GCS_SERVICE_ACCOUNT_JSON"), "base64").toString("utf8")
-        ),
+module.exports = ({ env }) => {
+  const gcsServiceAccount = env("GCS_SERVICE_ACCOUNT_JSON")
+    ? JSON.parse(Buffer.from(env("GCS_SERVICE_ACCOUNT_JSON"), "base64").toString("utf8"))
+    : undefined;
+
+  return {
+    upload: {
+      config: {
+        provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+        providerOptions: {
+          bucketName: env("GCS_BUCKET_NAME"),
+          publicFiles: true,
+          uniform: false,
+          basePath: "",
+          serviceAccount: gcsServiceAccount,
+        },
       },
     },
-  },
-});
+  };
+};
